1e54eb53bc6bf76879674555929d1fb90deb8921
Kapil Athalye <akapil@oath.com>
2018-02-14 17:28:06 -0800

    Adding python scripts

--- python/python_scripts/ysb_find_unique_apis2.py
+++ python/python_scripts/ysb_find_unique_apis2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-##./ysb_find_unique_apis.py 'URI=(.*?)\s\|\sTime Taken : (.*?)ms' ../testdata_python/bill-2018-01-11.log
 
 import sys, re
 
@@ -16,8 +15,6 @@ f = open(fileName, "r")
 
 for line in iter(f):
 
-    ##[INFO ] 2018-01-11 00:14:24.436 [qtp101478235-44] LogFilter - Response: | REQUEST ID=1353 | URI=/subscriptionservice/v1/users/saket.pathak08@gmail.com%23vj/unpaidinvoices | Time Taken : 2ms; | PAYLOAD=[] 
-    ## Pattern : 'URI=(.*?)\s\|\sTime Taken : (.*?)ms'
   
     regex = re.compile(pattern) 
     match = regex.search(line)
@@ -35,45 +32,5 @@ for key in sorted(data.iterkeys()):
 
 
 
-
-###TEMP
-## scp akapil@zbill103.qa.sbs.bf1.yahoo.com:~/bill-2018-01-11.log ~/
-'''
->>> import sys, re
->>> pattern = 'URI=(.*?)\s\|\sTime Taken : (.*?)ms'
->>> regex = re.compile(pattern) 
->>> data={}
->>> 
->>> for x in range(1,5):
-        print "Loop:", x
-...     line=raw_input("Enter string:")
-...     match = regex.search(line)
-...     if match:
-...         print match.groups()
-...         key = match.group(1)
-            print "existing value:", data.get(key, 0)
-            value = data.get(key, 0) + 1
-...         data.update({key:value})
-...         print data
-
-'''
-
-'(\/\w+)?'
-
-
-
-####Prog 2
-'''
-import sys, re
-for x in range(1,5):
-    line=raw_input("Enter string:")
-    pattern=raw_input("Enter pattern:")
-    match=re.search(pattern,line)
-    if match:
-        print "Found match..."
-        print match.group()
-
-
-'''
 
 
